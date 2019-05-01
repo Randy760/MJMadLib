938b15832f9e412247a33be6caaa35ff67b666ab
Randy760 <36725397+Randy760@users.noreply.github.com>
2019-04-30 23:55:08 -0700

    Add files via upload

---
+++ MJmadlibs.py
@@ -0,0 +1,28 @@
+
+print('Lets play MadLibs!')
+print('Give me a number.')
+number = input()
+print('Give me a noun.')
+noun_one = input()
+print('Give me a verb.')
+verb_one = input()
+print('Give me a noun, again.')
+noun_two = input()
+print('last but not least, give me another verb.')
+verb_two = input()
+
+
+
+print('Ive missed more than ' + str(number) + ' shots in my career.')
+print('Ive lost almost ' + str(300) + ' ' +  str(noun_one) +'s')
+print(str(26) + ' times, Ive been trusted to ' + str(verb_one) + ' the game winning ' + str(noun_two) + ' and missed.')
+print('Ive failed over and over and over again in my life.')
+print('And that is why I ' + str(verb_two) +  '.' )
+print('- Michael Jordan')
+
+
+ 
+
+
+
+
